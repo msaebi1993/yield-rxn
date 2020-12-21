fixed exploding gradient issue with lr scheduler

rxntorch/containers/.ipynb_checkpoints/molecule-checkpoint.py
@@ -9,29 +9,50 @@ class Mol(object):
     """
     def __init__(self, mol_json):
         self.smile = mol_json.get('smiles','')
-        self.name= mol_json.get('name','')
-        self.category=mol_json.get('category','')
-        self.molecular_weight=mol_json.get('molecular_weight',0.0)
-        self.volume=mol_json.get('volume',0.0)
-        self.surface_area=mol_json.get('surface_area',0.0)
-        self.ovality=mol_json.get('ovality',0.0)
-        self.hardness=mol_json.get('hardness',0.0)
-        self.dipole_moment=mol_json.get('dipole_moment',0.0)
-        self.vib_modes=mol_json.get('vib_modes',[[0.0,0.0],[0.0,0.0],[0.0,0.0]])
+        self.name = mol_json.get('name','')
+        self.category = mol_json.get('category','')
+        """
+        self.molecular_weight = float(mol_json.get('molecular_weight',0.0))
+        self.volume = float(mol_json.get('volume',0.0))
+        self.surface_area = float(mol_json.get('surface_area',0.0))
+        self.ovality = float(mol_json.get('ovality',0.0))
+        self.hardness = float(mol_json.get('hardness',0.0))
+        self.dipole_moment = float(mol_json.get('dipole_moment',0.0))
+        self.vib_modes = mol_json.get('vib_modes',[[0.0,0.0],[0.0,0.0],[0.0,0.0]])
         
-        self.electronegativity=mol_json.get('electronegativity',0.0)
-        self.HOMO_energy=mol_json.get('HOMO_energy',0.0)
-        self.LUMO_energy=mol_json.get('LUMO_energy',0.0)
-        self.atoms=mol_json.get('atoms',{})
-        self.atoms_nums=len(self.atoms)
-        self.attributes=torch.tensor([[self.molecular_weight,self.volume,
+        self.electronegativity = float(mol_json.get('electronegativity',0.0))
+        self.HOMO_energy = float(mol_json.get('HOMO_energy',0.0))
+        self.LUMO_energy = float(mol_json.get('LUMO_energy',0.0))
+        self.atoms = mol_json.get('atoms',{})
+        self.atoms_nums = len(self.atoms)
+        """
+        self.molecular_weight = self.get_func(mol_json,'molecular_weight')
+        self.volume = self.get_func(mol_json,'volume')
+        self.surface_area = self.get_func(mol_json,'surface_area')
+        self.ovality = self.get_func(mol_json,'ovality')
+        self.hardness = self.get_func(mol_json,'hardness')
+        self.dipole_moment = self.get_func(mol_json,'dipole_moment')
+        self.vib_modes = mol_json.get('vib_modes',[[0.0,0.0],[0.0,0.0],[0.0,0.0]])
+        
+        self.electronegativity = self.get_func(mol_json,'electronegativity')
+        self.HOMO_energy = self.get_func(mol_json,'HOMO_energy')
+        self.LUMO_energy = self.get_func(mol_json,'LUMO_energy')
+        self.atoms = mol_json.get('atoms',{})
+        self.atoms_nums = len(self.atoms)
+
+        
+        self.attributes = torch.tensor([[self.molecular_weight,self.volume,
                                  self.surface_area,self.ovality,
                                  self.hardness,self.dipole_moment,
                                  self.electronegativity,self.HOMO_energy,self.LUMO_energy]])
         
-        #self.mol_obj=
 
-    
+    def get_func(self,mol_json,key):
+        if mol_json.get(key,0) is None:
+            return -1
+        else:
+            return float(mol_json.get(key,0))
+        
     def get_attributes(self):
         #print(attributes)
         return self.attributes

rxntorch/models/.ipynb_checkpoints/yield_network-checkpoint.py
@@ -53,7 +53,8 @@ class YieldTrainer(nn.Module):
         self.pos_weight = pos_weight
         self.total_iters = 0
         self.optimizer = opt.Adam(self.model.parameters(), lr=self.lr, betas=betas, weight_decay=weight_decay)
-
+        #self.lr_scheduler = opt.lr_scheduler.ExponentialLR(optimizer=self.optimizer, gamma=1-self.lr_decay)
+        self.lr_scheduler = opt.lr_scheduler.ReduceLROnPlateau(self.optimizer, patience=5, verbose=True,factor=self.lr_decay,min_lr=1e-6,)
     def train_epoch(self, epoch, data_loader):
         logging.info("{:-^80}".format("Training"))
         self.model.train()
@@ -116,8 +117,8 @@ class YieldTrainer(nn.Module):
             pred_yields=np.append(pred_yields,bb,0)
                 
             if aa.shape !=bb.shape or correct_yields.shape != pred_yields.shape:
-                print(aa.shape ,bb.shape )
-                print(correct_yields.shape ,pred_yields.shape)
+                #print(aa.shape ,bb.shape )
+                #print(correct_yields.shape ,pred_yields.shape)
                 raise ValueError("Found input variables with inconsistent numbers of elements")
                 
             tmp_r2=r2_score(correct_yields,pred_yields)
@@ -125,7 +126,7 @@ class YieldTrainer(nn.Module):
                 
             tmp_r2=0 if math.isnan(tmp_r2) else tmp_r2
             cum_r2=0 if math.isnan(cum_r2) else cum_r2
-
+            learning_rate=np.mean([ group['lr'] for group in self.optimizer.param_groups ])
             if train:
                 self.total_iters += 1
                 self.optimizer.zero_grad()
@@ -137,7 +138,9 @@ class YieldTrainer(nn.Module):
                 if self.grad_clip is not None:
                     nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)
                 self.optimizer.step()
-           
+                
+                #self.lr_scheduler.get_lr()
+                
             if (i+1) % self.log_freq == 0:
                 if train:
                     post_fix = {
@@ -146,11 +149,12 @@ class YieldTrainer(nn.Module):
                             "iters": iters,
                             "avg_loss": avg_loss,
                             "r2": tmp_r2,
-                            "cum_r2":cum_r2/i,
+                            #"cum_r2":cum_r2/i,
                             "pnorm": param_norm,
-                            "gnorm": sum_gnorm
+                            "gnorm": sum_gnorm,
+                            "learning_rate":learning_rate
                     }
-                    logging.info(("Epoch: {epoch:2d}  Iter: {iter:5d}  Loss: {avg_loss:7.5f}  R2: {r2:6.2%}  Cum_R2:{cum_r2:6.2%} " 
+                    logging.info(("Epoch: {epoch:2d}  Iter: {iter:5d}  Loss: {avg_loss:7.5f}  R2: {r2:6.2%}  LR:{learning_rate:8.8f} " 
                             "Param norm: {pnorm:8.4f}  Grad norm: {gnorm:8.4f}").format(
                             **post_fix))
                             
@@ -164,27 +168,30 @@ class YieldTrainer(nn.Module):
                             "iters": iters,
                             "avg_loss": avg_loss,
                             "r2": tmp_r2,
-                            "cum_r2":cum_r2/i
+                            #"cum_r2":cum_r2/i
+                            "learning_rate": learning_rate
                         }
-                    logging.info(("Epoch: {epoch:2d}  Iter: {iter:5d}  Loss: {avg_loss:7.5f}  R2: {r2:6.2%}  Cum_R2:{cum_r2:6.2%} ").format(
+                    logging.info(("Epoch: {epoch:2d}  Iter: {iter:5d}  Loss: {avg_loss:7.5f}  R2: {r2:6.2%}  LR:{learning_rate:8.8f} ").format(
                             **post_fix))
                     #sum_acc10 = sum_acc12 = sum_acc16 = sum_acc20 = sum_acc40 = sum_acc80 = 0.0
                     
                 avg_loss = 0.0
                 sum_gnorm = 0.0
-                
+            """ 
             if self.total_iters % self.lr_steps == 0:
                 for param_group in self.optimizer.param_groups:
                     param_group['lr'] *= self.lr_decay
                 logging.info("Learning rate changed to {:f}".format(self.optimizer.param_groups[0]['lr']))
-        
+            """   
         if not train:
-            logging.info("Epoch: {:2d}  Loss: {:f}  R2: {:6.2%}  Cum_R2:{:6.2%}  ".format(epoch,test_loss, tmp_r2,cum_r2/i))
+            logging.info("Epoch: {:2d}  Loss: {:f}  R2: {:6.2%}  LR:{:8.8f}  ".format(epoch,test_loss, tmp_r2,learning_rate))
             return correct_yields, pred_yields, tmp_r2, test_loss
             
         if train:
+            self.lr_scheduler.step(avg_loss)
             return tmp_r2,avg_loss
-
+        #self.lr_scheduler.step(avg_loss)
+        
             
     def save(self, epoch, filename, path):
         filename = filename + ".ep%d" % epoch
@@ -192,3 +199,4 @@ class YieldTrainer(nn.Module):
         torch.save(self.model.cpu(), output)
         self.model.to(self.device)
         logging.info("Model saved to {} in {}:".format(filename, path))
+