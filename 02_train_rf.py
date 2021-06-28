fixed score calculation, added more metrics

02_train_rf.py
@@ -22,6 +22,10 @@ import matplotlib.pyplot as plt
 from sklearn.exceptions import DataConversionWarning
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import mean_squared_error as s_rmse
+from sklearn.metrics import mean_absolute_error as s_mae
+
+
 from sklearn.preprocessing import StandardScaler
 
 warnings.filterwarnings("ignore")
@@ -34,7 +38,7 @@ parser.add_argument("-od", "--output_dir", default='rf_results', type=str, help=
 parser.add_argument("-th", "--feat_threshold",  type=float, default=0.0001, help="Threshold for feature importance to discard features")
 parser.add_argument("-ne", "--n_estimators",  type=float, default=1000, help="Number of trees in RF model")
 parser.add_argument("-md", "--max_depth",  type=float, default=10, help="Max depth in RF trees")
-parser.add_argument("-rs", "--random_state",  type=float, default=0, help="Random state for RF model")
+parser.add_argument("-rs", "--random_state",  type=int, default=0, help="Random state for RF model")
 parser.add_argument("-plt", "--plot_yield_dist",  type=bool, default=False, help="Plot the yield distribution")
 args = parser.parse_args()
 
@@ -43,21 +47,22 @@ args = parser.parse_args()
 data_type=args.dataset_name
 use_rdkit_features= args.use_rdkit_feats
 ext = '_'+ use_rdkit_features
-
+processed= 'processed_12'#+str(args.random_state)
 #inputs
-processed_path = os.path.join(args.dataset_path,data_type,'processed')
+processed_path = os.path.join(args.dataset_path,data_type,processed)
 
 input_data_file = os.path.join(processed_path,''.join([data_type,ext ,'.csv']))
 input_split_idx_file = os.path.join(processed_path,'train_test_idxs.pickle')
 
 
 #outputs
-output_path = os.path.join(args.dataset_path,data_type,args.output_dir)
+output_path = os.path.join(args.dataset_path,data_type,processed,args.output_dir)
 if not os.path.exists(output_path):
     os.mkdir(output_path)
 features_fn= os.path.join(output_path,'selected_feats.txt')
-results_fn= os.path.join(output_path,'rf_results'+ ext+'.csv')
-
+r2_fn= os.path.join(output_path,'rf_results_r2'+ ext+'.csv')
+mae_fn = os.path.join(output_path,'rf_results_mae'+ ext+'.csv')
+rmse_fn = os.path.join(output_path,'rf_results_rmse'+ ext+'.csv')
 
 print("\n\nReading data from: ",input_data_file)
 print("Using rdkit features!") if use_rdkit_features=='rdkit' else print("Not using rdkit features!")
@@ -126,24 +131,32 @@ def get_sorted_feat_importances(feat_names,feat_importances):
 
 
 selected_features=set()
-result_dict=defaultdict(lambda: defaultdict(list))
+result_dict=defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 with open(input_split_idx_file, 'rb') as handle:
     idx_dict = pickle.load(handle)
 
 print()
 for split_set_num in range(1,len(idx_dict['train_idx'])+1):
+#for split_set_num in range(1,3):
+
     
-    result_dict[split_set_num]['model_num']=split_set_num 
+    result_dict['r2'][split_set_num]['model_num']=split_set_num 
+    result_dict['mae'][split_set_num]['model_num']=split_set_num 
+    result_dict['rmse'][split_set_num]['model_num']=split_set_num 
     
     x_train_scaled, x_test_scaled , y_train, y_test= split_scale_data(df,split_set_num,idx_dict,'yield')
     rf = RandomForestRegressor(n_estimators = args.n_estimators, random_state = args.random_state ,max_depth=args.max_depth)
+    y_test ,y_train = y_test/100 ,y_train/100
+
     rf.fit(x_train_scaled ,y_train)
     
     
     feat_names = list(x_train_scaled.columns) ##get the top features
     sorted_feat_names, sorted_feat_importances = get_sorted_feat_importances(feat_names,rf.feature_importances_)    
     for feat,score in sorted_feat_importances:
-        result_dict[split_set_num][feat]=score
+        result_dict['r2'][split_set_num][feat]=score
+        result_dict['mae'][split_set_num][feat]=score
+        result_dict['rmse'][split_set_num][feat]=score
         if score >= args.feat_threshold:
             selected_features.add(feat)
 
@@ -153,25 +166,52 @@ for split_set_num in range(1,len(idx_dict['train_idx'])+1):
     train_r2 = round(100*r2_score(y_train,y_pred_train),3)
     test_r2 = round(100*r2_score(y_test,y_pred_test ),3)
     
-    result_dict[split_set_num]['train_r2'] = round(r2_score(y_train,y_pred_train),3)
-    result_dict[split_set_num]['test_r2'] = round(r2_score(y_test,y_pred_test ),3)
-    
+    result_dict['r2'][split_set_num]['train'] = round(r2_score(y_train,y_pred_train),3)
+    result_dict['r2'][split_set_num]['test'] = round(r2_score(y_test,y_pred_test ),3)
+
+    result_dict['mae'][split_set_num]['train'] = round(s_mae(y_train,y_pred_train),3)
+    result_dict['mae'][split_set_num]['test'] = round(s_mae(y_test,y_pred_test ),3)
+
+    result_dict['rmse'][split_set_num]['train'] = round(np.sqrt(s_rmse(y_train,y_pred_train)),3)
+    result_dict['rmse'][split_set_num]['test'] = round(np.sqrt(s_rmse(y_test,y_pred_test )),3)
+
+
     print(f'\nModel number: {split_set_num}')
-    print(f'Mean Absolute Train R2: { train_r2}')
-    print(f'Mean Absolute Test R2: { test_r2}')
+    print(f'Mean Absolute Train R2: ',100*result_dict['r2'][split_set_num]['train'])
+    print(f'Mean Absolute Test R2: ',100*result_dict['r2'][split_set_num]['test'])
+    
+    print(f'\nMean Absolute Train RMSE: ',result_dict['rmse'][split_set_num]['train'])
+    print(f'Mean Absolute Test RMSE: ',result_dict['rmse'][split_set_num]['test'])
+    
+    print(f'\nMean Absolute Train MAE: ',result_dict['mae'][split_set_num]['train'])
+    print(f'Mean Absolute Test MAE: ',result_dict['mae'][split_set_num]['test'])
 
 
     
 #Write the model summary results and selecyed features
-dp= pd.DataFrame.from_dict(result_dict, orient='index',columns=['model_num','train_r2','test_r2']+sorted_feat_names)
-summary= dp.describe().loc[['mean','std']]
-dp.append(summary)
+dp_r2= pd.DataFrame.from_dict(result_dict['r2'], orient='index',columns=['model_num','train','test']+sorted_feat_names)
+summary= dp_r2.describe().loc[['mean','std']]
+dp_r2= dp_r2.append(summary)
+
+
+dp_mae= pd.DataFrame.from_dict(result_dict['mae'], orient='index',columns=['model_num','train','test']+sorted_feat_names)
+summary= dp_mae.describe().loc[['mean','std']]
+dp_mae= dp_mae.append(summary)
+
+
+
+dp_rmse= pd.DataFrame.from_dict(result_dict['rmse'], orient='index',columns=['model_num','train','test']+sorted_feat_names)
+summary= dp_rmse.describe().loc[['mean','std']]
+dp_rmse = dp_rmse.append(summary)
 
 
 print(f"\nWriting selected features to: {features_fn}")
-print(f"Writing RFs summary results to: {results_fn}")
+print(f"Writing RFs summary results to:\n{r2_fn},\n{mae_fn},\n{rmse_fn}")
 
-dp.to_csv(results_fn)
+dp_r2.to_csv(r2_fn)
+dp_mae.to_csv(mae_fn)
+dp_rmse.to_csv(rmse_fn)
+
 if use_rdkit_features=='rdkit':
     with open(features_fn,'w') as f:
         f.write(','.join(map(str,selected_features)))

rxntorch/models/.ipynb_checkpoints/yield_scoring-checkpoint.py
@@ -41,7 +41,7 @@ class YieldScoring(nn.Module):
 
             score_g_avg=torch.mean(torch.abs(score_g),dim=1)
             score_d_avg=torch.abs(score_d)
-            
+            #final_score=(score_d_avg+score_g_avg)/2
             final_score=self.finalscore(torch.cat([score_d_avg,score_g_avg],dim=1))
             return final_score
         else:

rxntorch/models/.ipynb_checkpoints/yield_scoring-try-checkpoint.py
@@ -17,6 +17,8 @@ class YieldScoring(nn.Module):
         self.domain= Linear(dmfeats_size, 1)
         
         self.finalscore = Linear(2,1)
+        self.absolute_val= abs_score
+        #self.dcscore = Linear(hidden_size, 1)
         
         
     def forward(self, local_features, global_features, binary, sparse_idx,domain_feats):
@@ -35,14 +37,31 @@ class YieldScoring(nn.Module):
         l1, l2 = domain_feats.shape        
 
         if self.use_domain=='no_rdkit' or self.use_domain=='rdkit':
+            #domain_features = self.domain( domain_feats)#.unsqueeze(1))
+            #score_d= self.dcscore(domain_features)
             
             score_d= self.domain( domain_feats)
             score_g = self.fcscore(features)
-
-            score_g_avg=torch.mean(torch.abs(score_g),dim=1)
-            score_d_avg=torch.abs(score_d)
+            #print(self.absolute_val)
+            #if self.absolute_val=='abs':
+            if self.absolute_val:
+                score_g_avg=torch.mean(torch.abs(score_g),dim=1)
+                score_d_avg=torch.abs(score_d)
+            elif self.absolute_val=='no_abs':
+                score_g_avg=torch.mean(score_g,dim=1)
+                score_d_avg=score_d
+                
+            elif self.absolute_val=='sig':
+                score_g_avg=torch.mean(torch.sigmoid(score_g),dim=1)
+                score_d_avg=torch.sigmoid(score_d)
+                
+            elif self.absolute_val=='relu':
+                score_g_avg=torch.mean(F.relu(score_g),dim=1)
+                score_d_avg=F.relu(score_d)
+            
             #final_score=(score_d_avg+score_g_avg)/2
             final_score=self.finalscore(torch.cat([score_d_avg,score_g_avg],dim=1))
+            #final_score = F.softmax(torch.tensor([score_d_avg,score_g_avg],dtype=torch.float))
             return final_score
         else:
             score_g = self.fcscore(features)

rxntorch/models/attention.py
@@ -20,9 +20,7 @@ class Attention(nn.Module):
         d1, d2, d3, d4 = local_features.shape
         l1, l2, l3, l4 = binary_features.shape
         
-        #padded_binary_feats= torch.zeros(d1, d2, d3, d4)
-        #padded_binary_feats[:, :l2, :l3, :]=self.fcbinary(binary_feats)
-        #padded_binary_feats = padded_binary_feats.to(self.device)
+
         padded_binary_feats = F.pad(input=binary_features, pad=(0,0 , 0,d3-l3, 0,d2-l2), mode='constant', value=0)
         
         attention_features = F.relu( local_features+ padded_binary_feats)
@@ -30,9 +28,5 @@ class Attention(nn.Module):
         
         attention_score = torch.sigmoid(self.fcattention(attention_features))
         global_feats = torch.sum(local_feats.unsqueeze(1) * attention_score, dim=2)
-        #global_pair1 = global_feats[sparse_idx[:,0],sparse_idx[:,1]]
-        #global_pair2 = global_feats[sparse_idx[:,0],sparse_idx[:,2]]
-        #global_pair = global_pair1 + global_pair2
-        #local_pair = local_pair[sparse_idx[:,0],sparse_idx[:,1],sparse_idx[:,2]]
-        #return local_pair, global_pair
+
         return global_feats 

rxntorch/models/yield_network.py
@@ -10,6 +10,8 @@ from .wln import WLNet
 from .attention import Attention
 from .yield_scoring import YieldScoring
 from sklearn.metrics import roc_auc_score,r2_score
+from sklearn.metrics import mean_squared_error as s_mse
+from sklearn.metrics import mean_absolute_error as s_mae
 
 class YieldNet(nn.Module):
     def __init__(self, depth, dropout, afeats_size, bfeats_size, hidden_size, binary_size,dmfeats_size,max_nbonds,use_domain,abs_score):
@@ -57,22 +59,22 @@ class YieldTrainer(nn.Module):
         logging.info(80*"*"+'\n')
         logging.info("{:-^80}".format("Training"))
         self.model.train()
-        r2 ,loss,w1,w2= self.iterate(epoch, data_loader, train=True,valid=False)
-        return r2,loss,w1,w2
+        r2 ,rmse,mae, w1,w2= self.iterate(epoch, data_loader, train=True,valid=False)
+        return r2,rmse,mae,w1,w2
 
     def test_epoch(self, epoch, data_loader):
         logging.info("{:-^80}".format("Testing"))
         with torch.no_grad():
             self.model.eval()
-            r2 ,loss= self.iterate(epoch, data_loader, train=False,valid=False)
-        return r2,loss
+            r2 ,rmse,mae= self.iterate(epoch, data_loader, train=False,valid=False)
+        return r2,rmse,mae
 
     def valid_epoch(self, epoch, data_loader):
         logging.info("{:-^80}".format("Validating"))
         with torch.no_grad():
             self.model.eval()
-        r2 ,loss= self.iterate(epoch, data_loader, train=False,valid=True)
-        return r2,loss
+        r2 ,rmse,mae= self.iterate(epoch, data_loader, train=False,valid=True)
+        return r2,rmse,mae
 
 
     def iterate(self, epoch, data_loader, train=True,valid=False):
@@ -82,8 +84,12 @@ class YieldTrainer(nn.Module):
         iters = len(data_loader)
         n_samples = len(data_loader.dataset)
         r2=0
-        correct_yields=np.array([[0]])
-        pred_yields=np.array([[0]])
+        #correct_yields=np.array([[0]])
+        #pred_yields=np.array([[0]])
+        correct_yields=[]
+        pred_yields=[]
+
+
         cum_r2=0
         for i, data in enumerate(data_loader):
             data = {key: value.to(self.device, non_blocking=True) for key, value in data.items()}
@@ -109,17 +115,19 @@ class YieldTrainer(nn.Module):
 
             aa=data['yield_label'].cpu().detach().numpy()
             bb=yield_scores.cpu().detach().numpy()
-            correct_yields=np.append(correct_yields,aa,0)
-            pred_yields=np.append(pred_yields,bb,0)
+            #correct_yields=np.append(correct_yields,aa,0)
+            #pred_yields=np.append(pred_yields,bb,0)
+            correct_yields.append(aa)
+            pred_yields.append(bb)
                 
-            if aa.shape !=bb.shape or correct_yields.shape != pred_yields.shape:
+            if aa.shape !=bb.shape or len(correct_yields)!= len(pred_yields):
                 raise ValueError("Found input variables with inconsistent numbers of elements")
                 
-            tmp_r2 = r2_score(correct_yields,pred_yields)
-            cum_r2 += r2_score(aa,bb)
+            #tmp_r2 = r2_score(correct_yields,pred_yields)
+            #cum_r2 += r2_score(aa,bb)
                 
-            tmp_r2=0 if math.isnan(tmp_r2) else tmp_r2
-            cum_r2=0 if math.isnan(cum_r2) else cum_r2
+            #tmp_r2=0 if math.isnan(tmp_r2) else tmp_r2
+            #cum_r2=0 if math.isnan(cum_r2) else cum_r2
             
             learning_rate=np.mean([ group['lr'] for group in self.optimizer.param_groups ])
             if train:
@@ -139,31 +147,42 @@ class YieldTrainer(nn.Module):
                     
                 avg_loss = 0.0
                 sum_gnorm = 0.0
-  
+        
+        y_actual, y_pred = np.concatenate(np.array(correct_yields)).squeeze(1), np.concatenate(np.array(pred_yields)).squeeze(1)
+        logging.info(f"y_actual, y_pred shape: {y_actual.shape},{y_pred.shape}")
+        rmse= np.sqrt(s_mse(y_actual, y_pred ))
+        mae= s_mae(y_actual, y_pred )
+        final_r2 = r2_score(y_actual, y_pred )
+        
         if valid:
-            logging.info("Epoch: {:2d}  valid Loss: {:f}  valid R2: {:6.2%}  LR:{:8.8f}  ".format(epoch,test_loss, tmp_r2,learning_rate))
-            return tmp_r2,avg_loss
+            #logging.info("Epoch: {:2d}  valid Loss: {:f}  valid R2: {:6.2%}  LR:{:8.8f}  ".format(epoch,test_loss, tmp_r2,learning_rate))
+            logging.info("Epoch: {:2d}  valid RMSE: {:f}  valid R2: {:6.2%}  valid MAE: {:6.2%}  LR:{:8.8f}  ".format(epoch,rmse,final_r2, mae, learning_rate))
+            #return tmp_r2,avg_loss
+            return final_r2,rmse,mae#,w1,w2
         
         if train:
+            
             self.lr_scheduler.step(avg_loss)# for Su, with domain
-            logging.info("Epoch: {:2d}  train Loss: {:f}  train R2: {:6.2%}  LR:{:8.8f} param norm: {:8.4f}  grad norm: {:8.4f} ".format(epoch,test_loss, tmp_r2,learning_rate,param_norm, sum_gnorm))
+            logging.info("Epoch: {:2d}  train RMSE: {:f}  train R2: {:6.2%}  train MAE: {:6.2%}  LR:{:8.8f} param norm: {:8.4f}  grad norm: {:8.4f} ".format(epoch,rmse, final_r2,mae, learning_rate,param_norm, sum_gnorm))
             
             learned_weights=self.model.module.yield_scoring.finalscore.weight.data
             w1,w2=learned_weights[0][0].item(),learned_weights[0][1].item()
             #w1,w2=0,0
-            return tmp_r2,avg_loss,w1,w2
+            #return tmp_r2,avg_loss,w1,w2
+            return final_r2,rmse,mae,w1,w2
         
         if not train and not valid:
             
-            logging.info("Epoch: {:2d}  Test Loss: {:f}  Test R2: {:6.2%}  Test LR:{:8.8f}  ".format(epoch,test_loss, tmp_r2,learning_rate))
-            return tmp_r2, test_loss
+            logging.info("Epoch: {:2d}  Test RMSE: {:f}  Test R2: {:6.2%}  test MAE: {:6.2%}  Test LR:{:8.8f}  ".format(epoch,rmse, final_r2,mae,learning_rate))
+            #return tmp_r2, test_loss
+            return final_r2,rmse,mae#,w1,w2
 
         
 
         
             
     def save(self, epoch, filename, path):
-        output_path=path + filename +'/yield.model'
+        output_path=os.path.join(path ,filename ,'yield.model')
         torch.save(self.model.module.cpu(), output_path)
         self.model.to(self.device)
         logging.info("Model saved to {}:".format(output_path))